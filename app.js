202b8ae2c4f541dd5ece5448ae67b9a37ba07416
Charishma24003 <charishmais003@gmail.com>
2025-08-15 18:04:21 +0530

    ..

--- app.js
+++ app.js
@@ -1,36 +1,61 @@
 const slides=document.querySelectorAll(".slide");
 let counter=0;
 // console.log(slides);
-slides.forEach(
+const firstClone=slides[0].cloneNode(true);
+document.querySelector("main").appendChild(firstClone);
+
+const allSlides=document.querySelectorAll(".slide");
+
+allSlides.forEach(
     (slide,index)=>{
         slide.style.left=`${index*100}%`;
     }
 
 );
 
-function nextSlide(){
-    counter=(counter+1)%slides.length;
-    slideImage();
-}
-
-function previousSlide(){
-    counter=(counter-1+slides.length)%slides.length;
-    slideImage();
-}
-
-function slideImage(){
-    slides.forEach(
+function slideImage(animate=true){
+    allSlides.forEach(
         slide=>{
+            slide.style.transition=animate?"transform 1s ease-in-out":"none";
             slide.style.transform=`translateX(-${counter*100}%)`;
         }
     );
 }
 
+function nextSlide(){
+    counter++;
+    slideImage();
+
+    if(counter==allSlides.length-1){
+        setTimeout(()=>{
+            counter=0;
+            slideImage(false);
+        },1000);
+    }
+}
+
+function previousSlide(){
+    if(counter==0){
+        counter=allSlides.length-1; // Go to the last slide
+        slideImage(false);
+    }
+    counter--;
+    slideImage();
+}
+  
+
+
+
 let autoSlide=setInterval(nextSlide,3000);
 
-document.querySelector(".nav1").addEventListener("mouseenter",()=>{
+document.querySelector("main").addEventListener("mouseenter",()=>{
     clearInterval(autoSlide);
 });
-document.querySelector(".nav1").addEventListener("mouseleave",()=>{
+document.querySelector("main").addEventListener("mouseleave",()=>{
     autoSlide=setInterval(nextSlide,3000);
 });
+
+function resetAutoSlide() {
+    clearInterval(autoSlide);
+    autoSlide = setInterval(nextSlide, 3000);
+}
